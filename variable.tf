d5f02bd724102afceaf966af1dda9be8309018eb
shruti-linux <131751004+shruti-linux@users.noreply.github.com>
2025-08-29 11:15:08 +0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,27 @@
 
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "credentials_file" {
+  description = "Path to GCP service account JSON key"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "GCP zone"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "public_key_path" {
+  description = "Path to SSH public key"
+  type        = string
+}
